3793561c1d97a4d864b604b62314fbe294b85160
eswsenthil <103065446+eswsenthil@users.noreply.github.com>
2025-05-20 16:59:15 +0300

    Add files via upload

---
+++ DemoAutoPointer/DemoAutoPointer/main.cpp
@@ -0,0 +1,22 @@
+//
+//  main.cpp
+//  DemoAutoPointer
+//
+//  Created by Tester on 10.5.2022.
+//
+
+#include <iostream>
+
+void demoAutoPointers()
+{
+    
+}
+
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    
+    demoAutoPointers();
+    
+    return 0;
+}
